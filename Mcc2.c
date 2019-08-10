519b2746a8148aa73f103fc3cee1ae26f9b7839e
MasatakeShirai <MasatakeShirai0814@gmail.com>
2019-08-10 23:11:17 +0900

    step4:Improved error message

--- Mcc2.c
+++ Mcc2.c
@@ -24,9 +24,15 @@ struct Token{
 
 Token *token;
 
-void error(char *fmt, ...){
+void error_at(char *loc, char *fmt, ...){
 	va_list ap;
 	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, " ");
+	fprintf(stderr, "^\n");
 	vfprintf(stderr, fmt, ap);
 	fprintf(stderr, "\n");
 	exit(1);
@@ -41,13 +47,13 @@ bool consume(char op){
 
 void expect(char op){
 	if(token->kind != TK_RESERVED || token->str[0] != op)
-		error("It is not '%c'", op);
+		error_at(token->str, "It is not '%c'", op);
 	token = token->next;
 }
 
 int expect_number(){
 	if(token->kind != TK_NUM)
-		error("It is not number");
+		error_at(token->str ,"It is not number");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -87,7 +93,7 @@ Token *tokenize(char *p){
 			continue;
 		}
 
-		error("It can't be tokenized");
+		error_at(token->str, "It can't be tokenized");
 	}
 
 	new_token(TK_EOF, cur, p);
